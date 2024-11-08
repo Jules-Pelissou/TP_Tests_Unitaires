11376c3598e4313c8f182027930fb749965e157e
Jules Pelissou <113369857+Jules-Pelissou@users.noreply.github.com>
2024-11-08 17:28:23 +0100

    Adding unitary tests

--- src/test/java/ticketmachine/TicketMachineTest.java
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -30,4 +30,69 @@ class TicketMachineTest {
 		// Les montants ont été correctement additionnés
 		assertEquals(10 + 20, machine.getBalance(), "La balance n'est pas correctement mise à jour");
 	}
+
+	@Test
+	// S3 : Ticket non imprimé si montant insuffisant
+	void ticketNotPrinted(){
+		machine.insertMoney(PRICE-1);
+		assertFalse(machine.printTicket(), "Pas assez d'argent, ne doit pas imprimer");
+	}
+
+	@Test
+	// S4 :
+	void ticketIsPrinted(){
+		machine.insertMoney(PRICE);
+		assertTrue(machine.printTicket(), "Assez d'argent, doit imprimer");
+	}
+
+	@Test
+	// S5 :
+	void balanceIsDecremented(){
+		machine.insertMoney(PRICE);
+		machine.printTicket();
+		assertEquals(0, machine.getBalance(), "La balance de la machine doit être décrémentée");
+	}
+
+	@Test
+	// S6 :
+	void totalIsUpdated(){
+		machine.insertMoney(PRICE);
+		machine.printTicket();
+		assertEquals(PRICE, machine.getTotal(), "Le total doit être updaté");
+	}
+
+	@Test
+	// S7 :
+	void refundIsOK(){
+		machine.insertMoney(PRICE);
+		assertEquals(50, machine.refund(), "La machine doit rendre la monaie");
+	}
+
+	@Test
+	// S8 :
+	void refundResetBalance(){
+		machine.insertMoney(PRICE);
+		machine.refund();
+		assertEquals(0, machine.getBalance(), "La balance doit être à 0");
+	}
+
+	@Test
+	// S9 :
+	void positiveMoney(){
+		try{
+			machine.insertMoney(-PRICE);
+			fail("Il est impossible de rentrer un nombre négatif");
+		}catch(IllegalArgumentException e){
+		}
+	}
+
+	@Test
+	// S10 :
+	void createNegativeMachines(){
+		try{
+			machine = new TicketMachine(-PRICE);
+			fail("Il est impossible de créer une machine ave cun prix négatif");
+		}catch(IllegalArgumentException e){
+		}
+	}
 }
